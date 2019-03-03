731e7e18e243322b48ef0742635ce82d36d00f88
Panos V <hello@alwayslivid.com>
2019-03-03 20:56:37 +0200

    Completely refactored code.

--- README.md
+++ README.md
@@ -2,7 +2,35 @@
 "Tiny Flower Beds" is a bot that posts a tiny flower bed on Twitter every few hours.
 It is licensed under the GPL license.
 
+## Credentials
+
+In order to use this bot, you'll need to have access to the Twitter API.
+
+You can request access and get your credentials [here.](https://dev.twitter.com)
+
+The bot will check for a file called `credentials.py`.
+
+`credentials.py` should have the following format:
+
+`
+CONSUMER_KEY = '<CONSUMER KEY>'
+CONSUMER_SECRET = '<CONSUMER SECRET>'
+ACCESS_KEY = '<ACCESS KEY>'
+ACCESS_SECRET = '<ACCESS SECRET>'
+`
+
+By default, this bot will prioritize environment variables over the `credentials.py` file.
+
+If you believe that environment variables are more convenient for you, then you will need to register the following keys:
+* CONSUMER_KEY
+* CONSUMER_SECRET
+* ACCESS_KEY
+* ACCESS_SECRET
+... and assign those keys to their respective values.
+
 ### Live Demo
 A live demo of my bot can be viewed here; https://twitter.com/tinyflowerbeds
+
+Should you host your own variation of this bot, please include the link to my account in the biog
 
 ### 🌹
--- bot.py
+++ bot.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from credentials import *
+'''
+@author: AlwaysLivid
+@description: Bot that posts a tiny flower bed on Twitter every few hours.
+'''
+
 import tweepy
 import time, random
-import logging, os                                                                                                                                                     
+import logging, os, textwrap
+import threading
 
 print("""
 TinyFlowerBeds
@@ -29,62 +34,113 @@ logging.basicConfig(
 )
 
 config = {
-    'limit_per_line': 5,
-    'lines': 3,
-    'hour': 3600, # minutes in an hour
-    'cooldown': 2, # hours to wait
-    'emojis': ["🌻", "💮", "🌺", "🌹", "🌼", "🌿", "🌿", "🌷"]
+    'limit_per_line': 4,
+    'lines': 4,
+    'hours_to_wait': 2,
+    'emojis': ["🌻", "🌱", "🌸", "🌷" "💮", "🌺", "🌹", "🌼", "🌿", "🌿", "🌷"]
 }
 
+hour_in_minutes = 3600
+cooldown = config['hours_to_wait'] * hour_in_minutes
 limit = config['lines'] * config['limit_per_line']
 
-def user_info(api, user): 
-    ''' 
-    Prints user information. 
-    
-    Also works as a way to check whether the credentials are valid without making a tweet.
-    '''
-    logging.info("Username: {}".format(api.get_user(user.id).screen_name))
-    logging.info("Display Name: {}".format(user.name))
-    logging.info("ID: {}".format(user.id))
+credential_list = ["CONSUMER_KEY", "CONSUMER_SECRET", "ACCESS_KEY", "ACCESS_SECRET"]
+use_environment_variables = bool
+use_file_variables = bool
+amount_of_credentials = len(credential_list)
+credential_counter = 0
 
-def generate_batch():
-    '''
-    Generates a string containing flower emojis, while creating a virtual flower bed in the form of a tweet!
-    '''
-    batch = []
-    counter = 0
-    while counter < limit:
-        if counter % config['limit_per_line'] == 0:
-            batch.append("\n")
-        counter += 1
-        batch.append(random.choice(config['emojis']))
-    tweet = ''.join(batch)
-    return tweet
+CONSUMER_KEY = str
+CONSUMER_SECRET = str
+ACCESS_KEY = str
+ACCESS_SECRET = str
 
-def main():
-    logging.info("Attempting to login!")
-    auth = tweepy.OAuthHandler(cons_key, cons_sec)
-    auth.set_access_token(access_key, access_sec)
-    api = tweepy.API(auth)
-    user = api.me()
-    user_info(api, user)
-    logging.info("Successfully logged in!")
-    while True:        
-        try:
-            # api.update_status(generate_batch())
-            logging.info("Tweeted out a new flower bed!")
-            logging.info("The next tweet is scheduled to be made in {0} minutes".format(config['hour']))
-            time.sleep(config['hour'] * config['cooldown'])
-        except tweepy.RateLimitError:
-            logging.critical("Tweeting failed due to ratelimit.")
-            time.sleep(config['hour'] * config['cooldown'])
+for credential in credential_list:
+    if credential_list[0] in os.environ:
+        credential_counter += 1
+
+if credential_counter == amount_of_credentials:
+    logging.info("All environment variables were found!")
+    use_environment_variables = True
+    use_file_variables = False
+else:
+    logging.warning("Environment variables were not successfully found!")
+    logging.info("Using credentials.py instead.")
+    use_environment_variables = False
+    use_file_variables = True
+
+if use_environment_variables == True:
+    logging.info("Using environment variables.")
+    CONSUMER_KEY = os.environ['CONSUMER_KEY']
+    CONSUMER_SECRET = os.environ['CONSUMER_SECRET']
+    ACCESS_KEY = os.environ['ACCESS_KEY']
+    ACCESS_SECRET = os.environ['ACCESS_SECRET']
+elif use_file_variables == True:
+    logging.info("Using file variables.")
+    try:
+        from credentials import *
+    except ImportError:
+        logging.critical("An error occured while importing the credentials from the credentials.py file.")
+        logging.critical("The bot will now shut down.")
+        logging.info("Please check the README.md file for more information.")
+        exit()
+
+class Bot:
+    def user_info(self, api, user): 
+        ''' 
+        Prints user information. 
+        
+        Also works as a way to check whether the credentials are valid without making a tweet.
+        '''
+        logging.info("Username: {}".format(api.get_user(user.id).screen_name))
+        logging.info("Display Name: {}".format(user.name))
+        logging.info("ID: {}".format(user.id))
+
+    def generate_batch(self):
+        '''
+        Generates a string containing flower emojis, while creating a virtual flower bed in the form of a tweet!
+        '''
+        batch = ''
+        counter = 0
+        while counter < limit:
+            batch += random.choice(config['emojis'])
+            counter += 1
+        tweet = '\n'.join(textwrap.wrap(batch, config['limit_per_line']))
+        return tweet
+
+    def tweet_loop(self, api):
+        '''
+        Function that generates and tweets a flower batch at a specified interval.
+        '''
+        while True:
+            try:
+                api.update_status(self.generate_batch())
+                logging.info("Tweeted out a new flower bed!")
+                logging.info("The next tweet is scheduled to be made in {} minutes".format(cooldown))
+                time.sleep(cooldown)
+            except tweepy.RateLimitError:
+                logging.critical("Tweeting failed due to ratelimit. Waiting {} more minutes.".format(cooldown))
+                time.sleep(cooldown)
+
+    def main(self):
+        '''
+        Main function that takes care of the authentication and the threading.
+        '''
+        logging.info("Attempting to login!")
+        auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+        auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+        api = tweepy.API(auth)
+        user = api.me()
+        self.user_info(api, user)
+        logging.info("Successfully logged in!")
+        self.tweet_loop(api)
 
 if __name__ == '__main__':
     try:
-        main()
+        TwitterBot = Bot()
+        TwitterBot.main()
     except tweepy.TweepError:
       logging.critical("Authentication Error!")
-      logging.info("Please validate your credentials in the  credentials.py file.")
+      logging.info("Please validate your credentials.")
       time.sleep(10)
       quit()
