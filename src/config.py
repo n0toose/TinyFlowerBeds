e5a3f6e7ad843993c7ca7d82c7cf92e08bcfc817
Karl <51362523+karl01101101@users.noreply.github.com>
2020-04-10 12:40:57 +0300

    changed the cooldown timer

--- src/config.py
+++ src/config.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import random
 
 config = {
     'limit_per_line': 4,
     'lines': 4,
-    'hours_to_wait': 72,
+    'hours_to_wait': random.randint(2,5)*24,
     'emojis': ["🌻", "🌱", "🌸", "🌷" "💮", "🌺", "🌹", "🌼", "🌿", "🌿", "🌷"]
 }
