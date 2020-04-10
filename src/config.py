90902e505e09e0a6f2ba59a7bc8be517a1e36025
Panagiotis Vasilopoulos <hello@alwayslivid.com>
2020-04-10 13:12:09 +0300

    Improved coding style

--- src/config.py
+++ src/config.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
 import random
 
 config = {
     'limit_per_line': 4,
     'lines': 4,
-    'hours_to_wait': random.randint(2,5)*24,
+    'hours_to_wait': random.randint(2, 5) * 24,
     'emojis': ["🌻", "🌱", "🌸", "🌷" "💮", "🌺", "🌹", "🌼", "🌿", "🌿", "🌷"]
 }
