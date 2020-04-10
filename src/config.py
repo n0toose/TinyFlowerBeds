65db7fcc8fc55689c44709cc1b1750e21701f7c9
Karl <51362523+karl01101101@users.noreply.github.com>
2020-04-10 19:04:43 +0300

    unnecessary comments removed

--- src/config.py
+++ src/config.py
@@ -6,7 +6,7 @@ import random
 config = {
     'limit_per_line': 4,
     'lines': 4,
-    'mininterval': 2, #please input minimum interval in DAYS
-    'maxinterval': 5, #please input minimum interval in DAYS
+    'mininterval': 2,
+    'maxinterval': 5,
     'emojis': ["🌻", "🌱", "🌸", "🌷", "💮", "🌺", "🌹", "🌼", "🌿", "🌿", "🌷"]
 }
